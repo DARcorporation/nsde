08cebd0ca69cb0960097ecaf2d4bbf3cca1f6fc4
Daniel de Vries <daniel.devries@darcorp.com>
2019-09-26 10:11:25 -0500

    Improved tests

--- tests/test_differential_evolution.py
+++ tests/test_differential_evolution.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pytest
-import itertools
 
 from differential_evolution import *
 
@@ -9,30 +8,16 @@ def paraboloid(x):
     return np.sum(x * x)
 
 
-@pytest.mark.parametrize(
-    "strategy, adaptivity",
-    list(
-        itertools.product(
-            [
-                "/".join(s)
-                for s in itertools.product(
-                    *[
-                        EvolutionStrategy.__mutation_strategies__.keys(),
-                        ["1", "2", "3"],
-                        EvolutionStrategy.__crossover_strategies__.keys(),
-                        EvolutionStrategy.__repair_strategies__.keys(),
-                    ]
-                )
-            ],
-            [0, 1, 2],
-        )
-    ),
-)
-def test_differential_evolution(strategy, adaptivity):
+@pytest.mark.parametrize("repair", EvolutionStrategy.__repair_strategies__.keys())
+@pytest.mark.parametrize("crossover", EvolutionStrategy.__crossover_strategies__.keys())
+@pytest.mark.parametrize("number", [1, 2, 3])
+@pytest.mark.parametrize("mutation", EvolutionStrategy.__mutation_strategies__.keys())
+@pytest.mark.parametrize("adaptivity", [0, 1, 2])
+def test_differential_evolution(mutation, number, crossover, repair, adaptivity):
     tol = 1e-8
     dim = 2
 
-    strategy = EvolutionStrategy(strategy)
+    strategy = EvolutionStrategy("/".join([mutation, str(number), crossover, repair]))
     de = DifferentialEvolution(strategy=strategy, tolx=tol, tolf=tol, adaptivity=adaptivity)
     de.init(paraboloid, bounds=[(-100, 100)] * dim)
 
--- tests/test_openmdao_driver.py
+++ tests/test_openmdao_driver.py
@@ -1,10 +1,14 @@
 import numpy as np
 import openmdao.api as om
+import pytest
 
-from differential_evolution import DifferentialEvolutionDriver
+from differential_evolution import *
 
 
-def problem(dim):
+@pytest.fixture
+def problem():
+    dim = 2
+
     prob = om.Problem()
     prob.model.add_subsystem('indeps', om.IndepVarComp('x', val=np.ones(dim)), promotes=['*'])
     prob.model.add_subsystem('objf', om.ExecComp('f = sum(x * x)', f=1., x=np.ones(dim)), promotes=['*'])
@@ -16,10 +20,22 @@ def problem(dim):
     return prob
 
 
-def test_openmdao_driver():
-    prob = problem(2)
-    prob.setup()
-    prob.run_driver()
+@pytest.mark.parametrize("repair", EvolutionStrategy.__repair_strategies__.keys())
+@pytest.mark.parametrize("crossover", EvolutionStrategy.__crossover_strategies__.keys())
+@pytest.mark.parametrize("number", [1, 2, 3])
+@pytest.mark.parametrize("mutation", EvolutionStrategy.__mutation_strategies__.keys())
+@pytest.mark.parametrize("adaptivity", [0, 1, 2])
+def test_openmdao_driver(problem, mutation, number, crossover, repair, adaptivity):
+    tol = 1e-8
 
-    assert np.all(prob['x'] < 1e-4)
-    assert prob['f'][0] < 1e-4
+    strategy = "/".join([mutation, str(number), crossover, repair])
+    problem.driver.options["strategy"] = strategy
+    problem.setup()
+    problem.run_driver()
+
+    assert problem.driver._de.dx < tol or problem.driver._de.df < tol
+
+    if strategy.split("/")[0] != "best":
+        # The "best" mutation strategy collapses prematurely sometimes, so we can't be sure this is always true
+        assert np.all(problem['x'] < 1e-4)
+        assert problem['f'][0] < 1e-4
