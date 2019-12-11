05ff7fdc50761dfe2f96cd5c33f718cac6b094d3
Daniel de Vries <daniel.devries@darcorp.com>
2019-12-11 15:57:23 -0600

    Fixed openmdao tests

--- tests/test_openmdao.py
+++ tests/test_openmdao.py
@@ -10,7 +10,6 @@ import os
 import pytest
 
 from nsde import NSDEDriver
-from . import all_strategies, get_strategy_designation
 
 
 def get_problem(dim=2):
@@ -41,39 +40,34 @@ def problem():
     prob.cleanup()
 
 
-def _go_problem(prob, *args):
-    prob.driver.options["strategy"] = get_strategy_designation(*args[:-1])
-    prob.driver.options["adaptivity"] = args[-1]
-    problem.setup()
-    problem.run_driver()
+def _go_problem(prob):
+    prob.setup()
+    prob.run_driver()
 
 
-@all_strategies
-def test_single_objective_unconstrained(problem, *args):
+def test_single_objective_unconstrained(problem):
     problem, dim = problem
-    _go_problem(problem, *args)
+    _go_problem(problem)
 
     assert problem["x"] == pytest.approx(0.0, abs=1e-1)
     assert problem["f"] == pytest.approx(0.0, abs=1e-1)
 
 
-@all_strategies
-def test_single_objective_constrained(problem, *args):
+def test_single_objective_constrained(problem):
     problem, dim = problem
 
     f_con = om.ExecComp("c = 1 - x[0]", c=0.0, x=np.ones(dim))
     problem.model.add_subsystem("con", f_con, promotes=["*"])
     problem.model.add_constraint("c", upper=0.0)
 
-    _go_problem(problem, *args)
+    _go_problem(problem)
 
     assert problem["x"][0] == pytest.approx(1.0, abs=1e-1)
     assert problem["x"][1:] == pytest.approx(0.0, abs=1e-1)
     assert problem["f"] == pytest.approx(1.0, abs=1e-2)
 
 
-@all_strategies
-def test_single_objective_vectorized_constraints(problem, *args):
+def test_single_objective_vectorized_constraints(problem):
     problem, dim = problem
 
     problem.model.add_subsystem(
@@ -83,7 +77,7 @@ def test_single_objective_vectorized_constraints(problem, *args):
         )
     problem.model.add_constraint("c", upper=np.zeros(dim))
 
-    _go_problem(problem, *args)
+    _go_problem(problem)
 
     assert problem["x"] == pytest.approx(1.0, abs=1e-1)
     assert problem["f"] == pytest.approx(dim, abs=1e-1)
@@ -95,8 +89,7 @@ def test_seed_specified_repeatability():
         assert prob.driver._seed == 11
 
         prob.driver.options["max_gen"] = 10
-        prob.setup()
-        prob.run_driver()
+        _go_problem(prob)
 
         if i == 0:
             x = prob["x"]
@@ -113,15 +106,14 @@ def test_custom_population_size(problem):
 
     n_pop = 11
     problem.driver.options["pop_size"] = n_pop
-    problem.setup()
-    problem.run_driver()
+    _go_problem(problem)
 
     de = problem.driver.get_de()
     assert de.n_pop == n_pop
     assert de.pop.shape[0] == n_pop
 
 
-def test_multi_objective_unconstrainted():
+def test_multi_objective_unconstrained():
     # The test problem is the multi-objective Schaffer Problem No.1
     problem = om.Problem()
     problem.model.add_subsystem(
@@ -137,8 +129,7 @@ def test_multi_objective_unconstrainted():
     problem.model.add_objective("f")
 
     problem.driver = NSDEDriver()
-    problem.setup()
-    problem.run_driver()
+    _go_problem(problem)
 
     # Check that the solution is on the known pareto front
     assert problem["f"][1] == pytest.approx((problem["f"][0] ** 0.5 - 2) ** 2, abs=1e-2)
